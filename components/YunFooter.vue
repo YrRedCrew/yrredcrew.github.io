e9d8cc4f80b863705a79a20bd56d97ac6f609788
YangRan <1144586076@qq.com>
2024-02-25 01:38:51 +0800

    更新

---
+++ components/YunFooter.vue
@@ -0,0 +1,17 @@
+<template>
+	<footer class="va-footer p-4 text-$va-c-text-light" text="center sm">
+		<div class="beian" m="y-2">
+			<a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener">等等就去备案</a>
+		</div>
+		<div class="copyright flex justify-center items-center gap-2" p="1">
+			<span> © 2016 - 2024</span>
+			<a class="inline-flex animate-pulse" href="https://redcrew.cn" target="_blank" title="YrRedCrew">
+				<div class="i-ri-cloud-line"></div>
+			</a>
+			<span>Yr</span>
+		</div>
+		<div class="powered" m="2">
+			<span>由 <a href="https://github.com/YunYouJun/valaxy" target="_blank" rel="noopener">Valaxy</a> v0.18.1 驱动</span> | <span>主题 - <a href="https://github.com/YunYouJun/valaxy/tree/main/packages/valaxy-theme-yun" title="valaxy-theme-yun" target="_blank">Yun</a> v0.18.1</span>
+		</div>
+	</footer>
+</template>
