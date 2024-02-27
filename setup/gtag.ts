62e0e81052225be03fea40f8744d40faae04c967
YangRan <1144586076@qq.com>
2024-02-27 14:23:02 +0800

    更新

--- setup/gtag.ts
+++ setup/gtag.ts
@@ -4,7 +4,7 @@ import type { UserModule } from 'valaxy'
 export const install: UserModule = ({ isClient, app, router }) => {
   if (isClient) {
     app.use(VueGtag, {
-      property: { id: 'GTM-MNW3MNTQ' },
+      property: { id: 'G-LXY1FBMJM6' },
     })
 
     trackRouter(router)
